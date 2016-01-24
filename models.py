87c5e3b967c9517ded163f629862c461a5973687
Roberto Nogueira <robertondc@gmail.com>
2016-01-24 18:13:02 -0200

    classes

---
+++ models.py
@@ -0,0 +1,32 @@
+# -*- conding: UTF-8 -*-
+
+class Perfil(object):
+	'Classe de usuario'
+
+	def __init__(self, nome, telefone, empresa):
+		self.nome = nome
+		self.telefone = telefone
+		self.empresa = empresa
+
+	def imprimir(self):
+		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+
+class Data(object):
+
+	def __init__(self, dia, mes, ano):
+		self.dia = dia
+		self.mes = mes
+		self.ano = ano
+
+	def imprimir(self):
+		print '%s/%s/%s' % (self.dia, self.mes, self.ano)
+
+class Pessoa(object):
+	def __init__(self, nome, peso, altura):
+		self.nome = nome
+		self.peso = peso
+		self.altura = altura
+
+	def imprime_imc(self):
+		imc = self.peso/(self.altura ** 2)
+		print 'Imc de %s: %s' % (self.nome, imc)
