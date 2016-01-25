8de53b8103e1ff3a0498c322cf7bbe0b9b442b8c
Roberto Nogueira <robertondc@gmail.com>
2016-01-25 20:36:02 -0200

    encapsulamento

--- models.py
+++ models.py
@@ -7,9 +7,17 @@ class Perfil(object):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
+		self.__curtidas = 0
 
 	def imprimir(self):
 		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+
+	def curtir(self):
+		self.__curtidas += 1
+
+	def obter_curtidas(self):
+		return self.__curtidas
+
 
 class Data(object):
 
